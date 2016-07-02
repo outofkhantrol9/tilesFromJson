0d3317eaf92845306733a0372bfbc11240d223fa
outofkhantrol9 <outofkhantrol9@gmail.com>
2016-07-01 19:36:30 -0700

    Create tilesFromJson.js

---
+++ tilesFromJson.js
@@ -0,0 +1,44 @@
+function tilesFromJson(json) {
+  json = JSON.parse(JSON.stringify(json));
+  var tiles = [];
+  tilesFromJson.layerIteration(json, tiles);
+  return tiles;
+}
+
+tilesFromJson.layerIteration = function layerIteration(json, tiles) {
+	json.layers.forEach(function(layer, i) {
+  	tilesFromJson.xIteration(json, tiles, i);
+  });
+  return tiles;
+};
+
+tilesFromJson.xIteration = function xIteration(json, tiles, i) {
+  for (var x = 0; x < json.width; x++) {
+    tilesFromJson.yIteration(json, tiles, i, x);
+  }
+  return tiles;
+};
+
+tilesFromJson.yIteration = function yIteration(json, tiles, i, x) {
+  for (var y = 0; y < json.height; y++) {
+    tilesFromJson.populateTiles(json, tiles, i, x, y);
+  }
+  return tiles;
+};
+
+tilesFromJson.populateTiles = function populateTiles(json, tiles, i, x, y) {
+  var type = json.layers[i].data[x + (y * json.width)];
+  if (type !== 0) {
+    tilesFromJson.addTile(json, tiles, i, x, y, type);
+  }
+  return tiles;
+};
+
+tilesFromJson.addTile = function addTile(json, tiles, i, x, y, type) {
+  tiles.push({
+    x: x * json.tilewidth,
+    y: y * json.tileheight,
+    type: type
+  });
+  return tiles;
+};
